34cb6946a114268c289b4b1e75b73b6d9ca86983
carmen <carmen.smith@uvm.edu>
2023-06-05 10:41:01 -0400

    add error handling

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let data = {};
 
-// Parse the CSV data
 fetch('codes.csv')
-    .then(response => response.text())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Could not fetch CSV data");
+        }
+        return response.text();
+    })
     .then(csvData => {
         let lines = csvData.split('\n');
         lines.forEach(line => {
@@ -11,19 +15,32 @@ fetch('codes.csv')
                 data[parts[0].trim()] = parts[1].trim();
             }
         });
+    })
+    .catch(error => {
+        // Handle any errors that occurred while fetching or parsing the CSV data.
+        console.error("Error: ", error);
+        alert("There was an error loading the page. Please try again later.");
     });
 
-// Handle form submission
+
 document.getElementById('codeForm').addEventListener('submit', function(event) {
     event.preventDefault();
     let codeInput = document.getElementById('codeInput').value;
     let link = data[codeInput];
+    let linkContainer = document.getElementById('linkContainer');
+    let linkInstructions = document.getElementById('linkInstructions');
+    let errorMessage = document.getElementById('errorMessage');
+    
     if(link) {
-        let linkContainer = document.getElementById('linkContainer');
-        let linkInstructions = document.getElementById('linkInstructions');
-
+        // Clear any previous error message.
+        errorMessage.classList.add('d-none');
         linkInstructions.classList.remove('d-none');
-        linkContainer.innerHTML = `<a href="${link}" target="_blank">Your Unique Study Materials</a>`;
+        linkContainer.innerHTML = `<a href="${link}" target="_blank">${link}</a>`;
+    } else {
+        // Show an error message if the code was not found.
+        linkInstructions.classList.add('d-none');
+        errorMessage.classList.remove('d-none');
     }
 });
+
 
